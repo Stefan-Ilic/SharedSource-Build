075d99f8d13ef9c9153de106de311b79a543fb34
Stefan Ilic <stefan.ilic@rubicon.eu>
2020-03-10 13:09:54 +0100

    Add first attempt of NUnit 3

---
+++ makenuget.cmd
@@ -0,0 +1,4 @@
+del "C:\NuGet\Remotion.BuildScript.2.0.0-alpha098.nupkg"
+rmdir /S /Q "C:\Development\IO\packages\Remotion.BuildScript.2.0.0-alpha098"
+rmdir /S /Q "C:\Development\trunk-svn2git\packages\Remotion.BuildScript.2.0.0-alpha098"
+packages\NuGet.CommandLine.2.8.6\tools\NuGet.exe pack BuildScript\BuildScript.csproj -verbosity detailed -version 2.0.0-alpha098 -OutputDirectory C:\nuget
